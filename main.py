0d9df3d79c2c4db70c5413fd542bd6c3d4268197
unknown <korsh624@gmail.com>
2024-03-07 18:07:37 +0300

    add arduino code

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route("/hardinfo", methods=['GET'])
+def hardinfo():
+    data = request.args.get('station')
+    print(data)
+    obj = {
+        "Value": data,
+        "Subject": "Data Structures and Algorithms",
+    }
+    return render_template('index.html', data=data),obj
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0')
