04ae25b2fa6635ca65807a10739a1f8ccd18dae3
korsh624 <korsh624@gmail.com>
2024-03-19 12:51:02 +0300

    add get request data

---
+++ db.py
@@ -0,0 +1,4 @@
+sql_login='gb_tech624'
+sql_name='gb_tech624'
+sql_host='mysql93.1gb.ru'
+sql_pass='WdujaqD2Mq-K'
--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from flask import Flask, render_template, request
+from dbmodel import DBmanager
+from db import sql_name,sql_host, sql_pass, sql_login
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('mainpage.html')
+
+
+@app.route("/senddata", methods=['GET'])
+def senddata():
+    table = request.args.get('table')
+    status = request.args.get('status')
+    mode = request.args.get('mode')
+    wait = request.args.get('wait')
+    count = request.args.get('count')
+    base = DBmanager(sql_host, sql_login, sql_pass, sql_name)
+    base.query(f"""CREATE TABLE IF NOT EXISTS table{table}(id INT AUTO_INCREMENT PRIMARY KEY, status text, mode text, wait text, count text)""")
+    base.query(f"""INSERT INTO table{table}(status, mode, wait, count) VALUES(%s, %s, %s, %s)""",(status, mode, wait, count))
+
+    return render_template('senddata.html')
+
+
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0')
